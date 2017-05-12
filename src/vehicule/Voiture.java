9467dcae6b6dd0e3cbe3f9b3de1e9a4bfb28ce26
StraetenMax <maxstraeten@yahoo.com>
2017-05-12 13:49:44 +0200

    correction id

--- src/vehicule/Voiture.java
+++ src/vehicule/Voiture.java
@@ -1,5 +1,25 @@
 package vehicule;
 
 public class Voiture {
+	//proprietés
+	private int id;
+	private String marque;
+	private double vitesse;
+	private int puissance;
+	private static int count;
 
+	//constructeurs
+	public Voiture(){
+		marque = "";
+		vitesse = 00.0;
+		puissance = 0;
+		
+	}
+	public Voiture(String cMarque, double cVitesse, int cPuissance){
+		id = ++count;
+		marque = cMarque;
+		vitesse = cVitesse;
+		puissance = cPuissance;
+	}
 }
+	
